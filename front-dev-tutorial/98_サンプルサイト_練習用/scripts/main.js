fc46a7a41ab2988622eb549c4dab4b0d4b37bf40
gottu52 <ryohei846@email.ne.jp>
2021-11-21 12:47:11 +0900

    .

--- front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
+++ front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
@@ -5,13 +5,23 @@ document.addEventListener('DOMContentLoaded', function () {
     hero.start();
 
     // インターセクションオブサーバー
-    const cb = function (el, isIntersecting) {
-        if(isIntersecting) {
+    const cb = function (el, inview) {
+        if(inview) {
             const ta = new TweenTextAnimation(el);
             ta.animate();
         }
     }
 
     const so = new ScrollObserver('.tween-animate-title', cb);
+
+    const _inviewAnimation = (el, inview) => {
+        if(inview) {
+            el.classList.add('inview');
+        } else {
+            el.classList.remove('inview');
+        }
+    }
+
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
 
